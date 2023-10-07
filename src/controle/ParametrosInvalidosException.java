a8c6760085c7d130b59e7f2f171d61692c5b4d53
MatosOliveira <matosalem.oliveira@gmail.com>
2023-10-07 13:30:07 -0300

    Projeto

---
+++ src/controle/ParametrosInvalidosException.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class ParametrosInvalidosException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}
+	
+	public ParametrosInvalidosException() {
+	}
+	
+}
